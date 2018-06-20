593c5abdc779634df5272b1842ce5fb29d4689ee
Nils Edvardsson <nilsedvardsson@outlook.com>
2018-06-20 08:13:44 +0200

    Test base for api call builder

---
+++ FunWithSpotifyApi/FunWithSpotifyApiTests/SpotifyApiCallBuilderTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FunWithSpotifyApi.Services;
+using NUnit.Framework;
+
+namespace FunWithSpotifyApiTests
+{
+    [TestFixture]
+    public class SpotifyApiCallBuilderTest
+    {
+        private SpotifyApiCallBuilder _builder;
+        [SetUp]
+        public void TestSetUp()
+        {
+            _builder = new SpotifyApiCallBuilder();
+        }
+    }
+}
